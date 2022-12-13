aecf5898fe6c96fd5335616401fe6793db8fd566
ArturSmith <akhmedmuzaev97@gmail.com>
2022-12-13 16:52:41 +0100

    Modul 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,20 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   var list = [
-    'assets/images/AngryCat.webp',
-    'assets/images/cat2.jpg',
-    'assets/images/cat3.jpg'
+    // 'assets/images/AngryCat.webp',
+    // 'assets/images/cat2.jpg',
+    // 'assets/images/cat3.jpg',
+    'assets/images/snake-venom-vase.svg'
   ];
   runApp(
     MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           centerTitle: true,
-          title: const Text("CATs"),
+          title: const Text("Snake venom vase"),
         ),
         body: CarouselSlider(
           options: CarouselOptions(height: double.infinity),
@@ -20,9 +22,7 @@ void main() {
             return Builder(
               builder: (BuildContext context) {
                 return Center(
-                  child: Container(
-                    child: Image.asset(i),
-                  ),
+                  child: SvgPicture.asset(i),
                 );
               },
             );
